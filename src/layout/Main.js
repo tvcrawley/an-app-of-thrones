15a970070041975d4f8156b98c48e91127a018e1
tvlangley <chibia2@gmail.com>
2019-01-28 17:27:58 -0500

    GET data from api
    - Restructure `state` (nested state is bad practice in react)
    - Use regex to send GET request to api
    - Store data from api as value for appropriate key
    - Loop through `state` to access/display each region

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -9,44 +9,40 @@ class Main extends Component {
     super()
 
     this.state = {
-      regions: [
-        {
-          "Dorne": []
-        },
-        {
-          "The North": []
-        },
-        {
-          "The Vale": []
-        },
-        {
-          "The Reach": []
-        },
-        {
-          "The Westerlands": []
-        },
-        {
-          "The Riverlands": []
-        },
-        {
-          "The Crownlands": []
-        },
-        {
-          "The Stormlands": []
-        },
-        {
-          "The Neck": []
-        },
-        {
-          "Iron Islands": []
+        "housesOfDorne": null,
+        "housesOfThe North": null,
+        "housesOfThe Vale": null,
+        "housesOfThe Reach": null,
+        "housesOfThe Westerlands": null,
+        "housesOfThe Riverlands": null,
+        "housesOfThe Crownlands": null,
+        "housesOfThe Stormlands": null,
+        "housesOfThe Neck": null,
+        "housesOfIron Islands": null
         }
-      ]
-    }
+      }
+
+  componentDidMount () {
+    const stateRegex = /(?<=housesOf).*/
+
+    Object.keys(this.state).forEach(key => {
+      if (stateRegex.test(key)) {
+        const regionName = key.match(stateRegex)[0]
+          fetch(`https://anapioficeandfire.com/api/houses?region=${regionName}`)
+          .then(res => res.ok ? res : new Error())
+          .then(res => res.json())
+          .then(res => this.setState({ [`housesOf${regionName}`]: res }))
+      }
+    })
   }
 
   render() {
-    const regions = this.state.regions.map(region => {
-      const regionName = Object.keys(region)[0]
+    console.log(this.state)
+    const stateRegex = /(?<=housesOf).*/
+
+    const regions = Object.keys(this.state).map(region => {
+      const regionName = region.match(stateRegex)[0]
+
       return <li key={regionName}>
                <Link to={`/regions/${regionName}`}> {regionName}</Link>
              </li>
