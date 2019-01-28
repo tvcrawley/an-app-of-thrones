9f311dc63f353255b13df18abe75e9c8a543f040
tvlangley <chibia2@gmail.com>
2019-01-28 10:21:46 -0500

    Add layout components
    Add header, main, and footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
+import Header from './layout/Header'
+import Main from './layout/Main'
+import Footer from './layout/Footer'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Header />
+        <Main />
+        <Footer />
       </div>
     )
   }
