281e77193f0aadbca893c78e21ba55f9a8d07296
tvlangley <chibia2@gmail.com>
2019-01-28 11:16:00 -0500

    Able to route to each region
    - Add `react-router`
    - Add `react-router-dom`
    - Add `withRouter` to allow App to have access to `match`, `location`,
    and `history` props
    - Add `HashRouter` to prevent issues when refreshing the webpage
    - Add routes for each region
    - Add clickable links for each region

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
 import './Main.css'
+// import Region from '../regions/Region'
 
 class Main extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      regions: [
+        {
+          "Dorne": []
+        },
+        {
+          "The North": []
+        },
+        {
+          "The Vale of Arryn": []
+        },
+        {
+          "The Reach": []
+        },
+        {
+          "The Riverlands": []
+        },
+        {
+          "Beyond the Wall": []
+        },
+        {
+          "The Stormlands": []
+        },
+        {
+          "Iron Islands": []
+        }
+      ]
+    }
+  }
+
   render() {
+    const regions = this.state.regions.map(region => {
+      const regionName = Object.keys(region)[0]
+      return <li key={regionName}>
+               <Link to={`/regions/${regionName}`}> {regionName}</Link>
+             </li>
+    })
     return (
       <div className="Main">
         Main
+        {regions}
+        <Route
+        path='/regions/:regionName'
+          // component={region.component}
+        />
       </div>
     )
   }
 }
 
-export default Main
+export default withRouter(Main)
