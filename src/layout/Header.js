f112f2016473abe4c477ee874b9b95950329e371
Toni Langley <tonia.langley@gmail.com>
2020-02-03 18:26:40 -0500

    Add home button
    - Enable user to navigate to the home page after leaving it

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-        Header
-      </div>
+      <header>
+        <Link to='/'>An App of Thrones</Link>
+      </header>
     )
   }
 }
