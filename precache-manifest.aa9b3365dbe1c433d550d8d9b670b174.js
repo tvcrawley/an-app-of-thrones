e68e690df2c923ad9f1c9d3971714fe44446993e
Toni Langley <tonia.langley@gmail.com>
2020-02-10 19:34:02 -0500

    Updates

---
+++ precache-manifest.aa9b3365dbe1c433d550d8d9b670b174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ac893ff9c9d43dd086ac30ff963dd0",
+    "url": "/an-app-of-thrones/index.html"
+  },
+  {
+    "revision": "af730bd2487d04bf68e9",
+    "url": "/an-app-of-thrones/static/css/main.40e3a3b5.chunk.css"
+  },
+  {
+    "revision": "573bd0ada04d645b2bc2",
+    "url": "/an-app-of-thrones/static/js/2.bde3c24b.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/an-app-of-thrones/static/js/2.bde3c24b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af730bd2487d04bf68e9",
+    "url": "/an-app-of-thrones/static/js/main.16b9d7ab.chunk.js"
+  },
+  {
+    "revision": "e417122099c9aed1e91a",
+    "url": "/an-app-of-thrones/static/js/runtime-main.bebf0b4c.js"
+  }
+]);
